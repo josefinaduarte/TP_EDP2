9f78ef3b9500d547944fc95c6fbdf372e80f5409
TiziRodriguez <129459699+TiziRodriguez@users.noreply.github.com>
2023-05-01 19:28:55 -0300

    actualizar cliente

--- ClaseCliente.py
+++ ClaseCliente.py
@@ -16,24 +16,40 @@ class Cliente(Persona):
     def Actualizar(self):
         campo=input('ingrese campo a actualizar')
         dato=input('ingrese el nuevo dato')
-        if campo=='nombre':
-            self.nombre=dato
-        elif campo=='DNI':
-            self.DNI=dato
-        elif campo=='genero':
-            self.genero=dato
-        elif campo=='usuario':
-            self.usuario=dato
-        elif campo=='contrasenia':
-            self.contrasenia=dato
-        elif campo=='telefono':
-            self.telefono=dato
-        elif campo=='direccion':
-            self.direccion=dato
-        elif campo=='antiguedad':
-            self.antiguedad=dato
-        else:
-            print('el campo ingresado no esta registrado')
+        cliente=input('ingrese DNI')
+        clientes=open("ListaClientes.txt",'r')
+        lista =clientes.readlines()
+        clientes.close()
+        cont=0
+        with open("ListaClientes.txt",'w')as archivo:  
+            for linea in lista:
+                if cliente in linea:
+                    rotulo=linea.split(',')
+                    if campo=='nombre':
+                        rotulo[0]=dato
+                    elif campo=='DNI':
+                        rotulo[1]=dato
+                    elif campo=='genero':
+                        rotulo[2]=dato  
+                    elif campo=='usuario':
+                        rotulo[3]=dato
+                    elif campo=='contrasenia':
+                        rotulo[4]=dato
+                    elif campo=='telefono':
+                        rotulo[5]=anio-dato
+                    elif campo=='direccion':
+                        rotulo[6]=dato
+                    elif campo=='anioIngreso':
+                        rotulo[7]=anio-dato
+                    else:
+                        print('el campo ingresado no esta registrado')
+                    cont=1
+                    lineaNueva =  ','.join(rotulo)
+                    archivo.write(lineaNueva)
+                else: 
+                    archivo.write(linea)
+            if cont==0:
+                print('el id ingresado no esta registrado')
     
     def __str__(self):
         return 'El cliente se llama {},su DNI es{}, su genero es {}, su usario es {}, su contrasenia es {}, su telefono es {}, su direccion es {} y su antiguedad es {}'.format(self.nombre,self.DNI,self.genero,self.usuario,self.contrasenia,self.telefono,self.direccion,self.antiguedad)
