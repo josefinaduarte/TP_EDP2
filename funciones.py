9e54f3993fcde75f4194881dd9ce2c8f69e1a588
TiziRodriguez <129459699+TiziRodriguez@users.noreply.github.com>
2023-05-03 21:00:18 -0300

    d

--- funciones.py
+++ funciones.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def extraerInfo (archivo):
         listaGen = []
         palabra = ''
@@ -66,3 +68,76 @@ def print_tabla( lista ):
         for dato in fila:
             print(dato, end='\t') 
         print()
+
+def validacionesgrales(campo,dato,listav,listaa,clientes,empleados):
+    if campo=='m2':
+        while not dato.isdigit():
+            dato=input("El ingreso no es valido. Ingrese el numero de metros cuadrados de la propiedad: ")
+    if campo=='id':
+        lista_ids=[]
+        for i in range(len(listav)):
+            lista_ids.append(listav[i][4])
+        for j in range(len(listaa)):
+            lista_ids.append(listaa[j][4])
+        while dato in lista_ids:
+            dato=str(randint(000,999))
+    if campo=='numambientes':
+        while not dato.isdigit():
+                dato=input("Hubo un error con el numero ingresado, intente nuevamente. Ingres el numero de ambientes de la propiedad: ")
+    if campo=='tipo':
+        while tipo not in "123":
+                tipo=input("Hubo un error con el numero ingresado. Ingrese 1 si la propiedad es un departamento,2 si es una casa, o 3 si es un local comercial: ")
+        if tipo=="1":
+            tipo="departamento"
+        elif tipo=="2":
+            tipo="casa"
+        else:
+            tipo="local"
+    if campo=='anionconstruccion':
+        while not dato.isdigit():
+            dato=input("Hubo un error con el valor ingresado. Ingrese el año de contruccion de la propiedad: ")
+    if campo=='estado':
+        while dato not in "12":
+                dato=input("Hubo un error con el valor ingresado. Ingrese 1 si desea poner su popiedad en venta, o 2 si desea ponerla en alquiler: ")
+        if dato=="1":
+            dato="en venta"
+            actividad="vender"
+        else:
+            dato="en alquiler"
+            actividad="alquilar"
+    if campo=='precio':
+        while not dato.isdigit():
+            dato=input("Hubo un error con el monto ingresado. Ingrese el monto en dolares al cual desea {} la propiedad: ".format(actividad))
+    if campo=='DNIempleado':
+        lista_dni=[]
+        for i in range(len(empleados)):
+            lista_dni.append(empleados[i][1])
+        while dato in lista_dnis:
+            dato=input("Hubo un error con el valor ingresado. Ingrese el dni del empleado: ")
+    if campo=='DNIcliente':
+        lista_dnis=[]
+        for i in range(len(clientes)):
+            lista_dnis.append(clientes[i][1])
+        while dato in lista_dnis:
+            dato=input("Hubo un error con el valor ingresado. Ingrese el dni del cliente: ")
+    if campo=='salario':
+        while not dato.isdigit():
+            dato=input("Hubo un error con el valor ingresado. Ingrese el salario del empleado: ")
+    if campo=='legajo':
+        while not dato.isdigit():
+            dato=input("Hubo un error con el valor ingresado. Ingrese el legajo del empleado: ")
+        lista_legajos=[]
+        for i in range(len(empleados)):
+            lista_dnis.append(empleados[i][1])
+        while dato in lista_legajos:
+            dato=input("Hubo un error con el valor ingresado. Ingrese el legajo del cliente: ")
+    if campo=='anioIngreso':
+         while not dato.isdigit():
+            dato=input("Hubo un error con el valor ingresado. Ingrese el anio de ingreso: ")
+    if campo=='usuario':
+        lista_usuarios=[]
+        for i in range(len(clientes)):
+            lista_dnis.append(clientes[i][5])
+        while dato in lista_dnis:
+            dato=input("Hubo un error con el valor ingresado. Ingrese el dni del cliente: ")
+    return dato
